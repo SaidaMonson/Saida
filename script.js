e27e5772e64c9864b9c9c90f17689175e5394690
Saida Guliyeva <monsonlaila@gmail.com>
2022-03-03 16:59:54 +0400

    второе задание

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const a = "Наша ветка lesson01"
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 124;
+let rollback = 99;
+let fullPrice = 100000;
+let adaptive = false;
 
-const logged = () => {
-    console.log("Наконец мы узнали все команды, необходимые для работы с GIT");
-}
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);   
+
+let conc = "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани";
+console.log(conc);             
+let conc2 = "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани";
+console.log(conc2);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+let n;
+n = fullPrice*(rollback/100);
+console.log(n);
