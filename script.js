311e16e977f5090229007dd1cb5b0ade7f321134
Saida Guliyeva <monsonlaila@gmail.com>
2022-03-03 15:32:18 +0400

    домашнее задание к первому уроку

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const a = "Наша ветка lesson01"
+let  title;
+let  screens;
+let  screenPrice;
+let  rollback;
+let  fullPrice;
+let  adaptive;
+alert("Закрыть");
+console.log("Здесь могла быть ваша реклама");
